8e0362c1859a71abd7b3bdcd1acad2a36ef9d3a2
anle20 <lehuuan18@gmail.com>
2024-09-16 17:18:35 +0700

    update README and script

--- README.md
+++ README.md
@@ -22,5 +22,6 @@ Optionally, you can also visit jenkins-elsa.example from your browser.
 
 Add a line to the bottom of the /etc/hosts file on your computer (you will need administrator access):
 127.0.0.1 jenkins-elsa.example
+127.0.0.1 hackathon-elsa.example
 $ minikube tunnel
 Now open the browser and enter jenkins-elsa.example
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 #Create sa and rolebinding for jenkins
-kubectl apply -f ../jenkins-service-account.yaml
-kubectl apply -f ../jenkins-role.yaml
-kubectl apply -f ../jenkins-rolebinding.yaml
+k apply -f ../jenkins-service-account.yaml
+k apply -f ../jenkins-role.yaml
+k apply -f ../jenkins-rolebinding.yaml
 
 #install jenkins
 helm repo add jenkins https://raw.githubusercontent.com/jenkinsci/kubernetes-operator/master/chart
 helm install jenkins jenkins/jenkins-operator -n jenkins-operator
 
 #get secret
-kubectl get secret jenkins-operator-credentials-jenkins -o 'jsonpath={.data.user}' | base64 -d
-kubectl get secret jenkins-operator-credentials-jenkins -o 'jsonpath={.data.password}' | base64 -d
+k get secret jenkins-operator-credentials-jenkins -o 'jsonpath={.data.user}' | base64 -d
+k get secret jenkins-operator-credentials-jenkins -o 'jsonpath={.data.password}' | base64 -d
+
+#create ingress
+k apply -f ../example-ingress.yaml
